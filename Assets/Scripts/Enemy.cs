82b3ec415a2b422cbf5a7aa1289391eea23e82d9
duykhoi1504 <voduykhoi1504@gmail.com>
2024-04-03 23:50:23 +0700

    add score

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,9 +8,16 @@ public class Enemy : MonoBehaviour
 
   [SerializeField] GameObject deathVFX;
   [SerializeField] Transform parent;
+
+  ScoreBoard score;
+  private void Start() {
+    score=FindObjectOfType<ScoreBoard>();
+  }
+  [SerializeField] int amountToIncrease=15;
   // Start is called before the first frame update
   private void OnParticleCollision(GameObject other)
   {
+    score.ScoreIncrease(amountToIncrease);
     GameObject vfx= Instantiate(deathVFX, this.transform.position, Quaternion.identity);
     vfx.transform.parent=parent;
     Destroy(this.gameObject);
